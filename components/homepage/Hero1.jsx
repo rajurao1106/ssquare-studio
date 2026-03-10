5a66286f97da0b4e800c087e44fac455c3452ddd
Lav <lavdigikraftsocial@gmail.com>
2026-03-10 17:50:38 +0530

    commit

--- components/homepage/Hero1.jsx
+++ components/homepage/Hero1.jsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import { Bodoni_Moda } from "next/font/google";
-import hero1 from "@/public/homepage/hero3.jpg"
-import hero35 from "@/public/homepage/hero3.5.png"
+import hero1 from "@/public/homepage/hero3.jpg";
+import hero35 from "@/public/homepage/hero3.5.png";
 
 const bodoni = Bodoni_Moda({
   subsets: ["latin"],
@@ -24,7 +24,7 @@ export default function Hero1() {
             fill
             priority
             className="object-cover"
-          /> 
+          />
           <Image
             src={hero35}
             alt="Modern Architecture"
@@ -39,20 +39,23 @@ export default function Hero1() {
           {/* Content */}
           <div className="relative z-10 flex h-screen justify-center items-center">
             <div className=" max-w-5xl flex justify-center items-center flex-col  px-6 md:px-16">
-              <h1 className="text-6xl text-center pb-50 font-bold md:text-7xl text-gray-800 font-[Bodoni_Moda] leading-tight">
-                Designing Spaces <br className="block max-lg:hidden"/>
-                That <span className="italic max-lg:text-4xl text-white">Inspire & Endure</span>
+              <h1 className="text-5xl text-center pb-50 font-bold md:text-7xl text-gray-800 font-[Bodoni_Moda]">
+                Designing Spaces <br className="block max-lg:hidden" />
+                That{" "}
+                <span className="italic max-lg:text-3xl text-white">
+                  Inspire & Endure
+                </span>
               </h1>
 
-             <div className="absolute backdrop-blur-md z-50 bg-white/20 py-5 pl-8 pb-5 right-5 bottom-5 rounded-lg">
-               <p className="mt-4 max-w-xl  mb-4 text-base md:text-lg">
-                We transform visions into timeless architecture, blending
-                innovative design with functional excellence.
-              </p>
-  <button className="bg-[#8FAF9A] hover:bg-[#7a9985] text-white px-8 py-3  transition-all duration-300 shadow-lg">
-                Contact Us
-              </button>
-             </div>
+              <div className="absolute backdrop-blur-md z-50 bg-white/20 py-5 pl-8 pb-5 right-5 bottom-5 rounded-lg">
+                <p className="mt-4 max-w-xl  mb-4 text-base md:text-lg">
+                  We transform visions into timeless architecture, blending
+                  innovative design with functional excellence.
+                </p>
+                <button className="bg-[#8FAF9A] hover:bg-[#7a9985] text-white px-8 py-3  transition-all duration-300 shadow-lg">
+                  Contact Us
+                </button>
+              </div>
               {/* <button className=" absolute bottom-0 left-0 rounded-tr-2xl bg-white px-8 py-3 text-sm font-semibold text-black transition hover:bg-gray-200">
               Schedule a Free Consultation
             </button> */}
